0190aa9db1bed0a6827e48e4476f9bbdcf2806c4
eldooon <eldonchan91@gmail.com>
2018-03-29 12:24:03 -0400

    Fixed formatting

--- Milo/Scenes/ListRecipe/ListRecipeViewController.swift
+++ Milo/Scenes/ListRecipe/ListRecipeViewController.swift
@@ -12,78 +12,72 @@
 
 import UIKit
 
-protocol ListRecipeDisplayLogic: class
-{
-  func displaySomething(viewModel: ListRecipe.FetchRecipes.ViewModel.DisplayedRecipes)
+protocol ListRecipeDisplayLogic: class {
+    func displaySomething(viewModel: ListRecipe.FetchRecipes.ViewModel.DisplayedRecipes)
 }
 
-class ListRecipeViewController: UICollectionViewController, ListRecipeDisplayLogic
-{
-  var interactor: ListRecipeBusinessLogic?
-  var router: (NSObjectProtocol & ListRecipeRoutingLogic & ListRecipeDataPassing)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup()
-  {
-    let viewController = self
-    let interactor = ListRecipeInteractor()
-    let presenter = ListRecipePresenter()
-    let router = ListRecipeRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+class ListRecipeViewController: UICollectionViewController, ListRecipeDisplayLogic {
+    var interactor: ListRecipeBusinessLogic?
+    var router: (NSObjectProtocol & ListRecipeRoutingLogic & ListRecipeDataPassing)?
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
     }
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    fetchRecipes()
-  }
-  
-  // MARK: Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
-  
-  func fetchRecipes()
-  {
-//    let request = Recipe.Something.Request()
-//    interactor?.doSomething(request: request)
-  }
-  
-  func displaySomething(viewModel: ListRecipe.FetchRecipes.ViewModel.DisplayedRecipes)
-  {
-    //nameTextField.text = viewModel.name
-  }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController = self
+        let interactor = ListRecipeInteractor()
+        let presenter = ListRecipePresenter()
+        let router = ListRecipeRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: Routing
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchRecipes()
+    }
+    
+    // MARK: Do something
+    
+    //@IBOutlet weak var nameTextField: UITextField!
+    
+    func fetchRecipes(){
+        //    let request = Recipe.Something.Request()
+        //    interactor?.doSomething(request: request)
+    }
+    
+    func displaySomething(viewModel: ListRecipe.FetchRecipes.ViewModel.DisplayedRecipes) {
+        //nameTextField.text = viewModel.name
+    }
+    
+    
 }
