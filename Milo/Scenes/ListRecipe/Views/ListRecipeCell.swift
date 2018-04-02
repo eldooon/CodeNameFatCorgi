76d4db40f7e790c1646e10a710846c0994c1bd9b
eldooon <eldonchan91@gmail.com>
2018-04-02 12:29:33 -0400

    Added Views Folder, with ListRecipeCell

---
+++ Milo/Scenes/ListRecipe/Views/ListRecipeCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListRecipeCell.Swift
+//  Milo
+//
+//  Created by Eldon Chan on 4/2/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class ListRecipeCell: UICollectionViewCell {
+    
+}
