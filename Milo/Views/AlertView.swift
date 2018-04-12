cb5197e3a5e1d7c406de131df0f9063158838b4a
eldooon <eldonchan91@gmail.com>
2018-04-12 14:24:27 -0400

    Added Alert xib and AlertView

---
+++ Milo/Views/AlertView.swift
@@ -0,0 +1,21 @@
+//
+//  AlertView.swift
+//  Milo
+//
+//  Created by Eldon Chan on 4/12/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class AlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
