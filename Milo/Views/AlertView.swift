eb2241e34d0961db5bd6b07c13cd1758786d61c4
eldooon <eldonchan91@gmail.com>
2018-04-12 14:34:19 -0400

    Added IBOutlet and init

--- Milo/Views/AlertView.swift
+++ Milo/Views/AlertView.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class AlertView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var alertImage: UIImageView!
+    @IBOutlet weak var alertLabel: UILabel!
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
